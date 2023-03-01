ab8874fa9739cd806746601da70185678cf979af
1MochaChan1 <danny8doo@gmail.com>
2023-03-01 09:57:12 +0530

    init commit

---
+++ Scripts/Player/movement.gd
@@ -0,0 +1,41 @@
+extends CharacterBody3D
+
+
+var speed:float = 5.0
+var mouse_sensitivity:float = 0.1
+var mouse_delta:Vector2=Vector2.ZERO
+
+
+
+@onready var yaw = $Yaw
+@onready var camera = $Yaw/Camera3D
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	handle_input()
+
+func handle_input():
+#	var z_axis = (Input.get_action_strength("back") - Input.get_action_strength("forward"))
+#	var x_axis = (Input.get_action_strength("strafe_right") - Input.get_action_strength("strafe_left"))
+#	•••••••• The below code is similar to the above code ••••••••
+	var input_dir := Input.get_vector("strafe_left", "strafe_right", "forward", "back")
+	var direction = (yaw.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() 
+	
+	if(direction):
+		velocity = Vector3(direction.x, velocity.y, direction.z) * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, 5)
+		velocity.z = move_toward(velocity.z, 0, 5)
+
+	move_and_slide()
+
+func _unhandled_input(event):
+	if(event is InputEventMouseMotion):
+		mouse_delta = event.relative
+		yaw.rotate_y(deg_to_rad(-mouse_delta.x * mouse_sensitivity))
+		camera.rotate_x(deg_to_rad(-mouse_delta.y*mouse_sensitivity))
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(40))
